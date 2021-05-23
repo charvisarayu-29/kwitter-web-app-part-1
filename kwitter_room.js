eba32cab492cd66d378a9bf5be56d6dd0d2fefd7
charvisarayu-29 <82302672+charvisarayu-29@users.noreply.github.com>
2021-05-23 09:27:30 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyALxNT1jJedcoDwXX0bCVH7HFGYt6xwBU4",
+    authDomain: "lets-chat-web-app-baf6f.firebaseapp.com",
+    projectId: "lets-chat-web-app-baf6f",
+    storageBucket: "lets-chat-web-app-baf6f.appspot.com",
+    messagingSenderId: "327564530832",
+    appId: "1:327564530832:web:4428eedb75ab1031b97415",
+    measurementId: "G-F1FWZESQG3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
